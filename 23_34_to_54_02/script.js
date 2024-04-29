d1135561deb78e7fa37fbc4e258ca7b7cdb4cb4c
Sihab Sarar Chistee <60611053+Sihab-10@users.noreply.github.com>
2024-04-29 15:06:59 +0600

    some naming rules added

--- 23_34_to_54_02/script.js
+++ 23_34_to_54_02/script.js
@@ -1,2 +1,47 @@
+"use strict";
+// use strict is help you to write error-free code.
+
 console.log("Hello World");
 // console.log can print somethng on console 
+
+//=================================== intro to variables===================================
+
+// variables can store some information
+// we can use that information later
+// we can change that information later
+
+// declare a variable
+// variable name is case-sensative
+
+var firstName = "Sihab";
+
+
+// use a variable 
+console.log(firstName);
+
+
+// change value 
+firstName = "Sifat";
+console.log(firstName);
+
+//==================================== rules for naming variable=========================//
+
+// you cannot start with number
+// example:-
+// 1value(invalid)
+// value1(valid)
+
+// you can use only underscore _ or dollar symbol $
+// first_name (valid)
+// _firstname (valid)
+
+// first$name (valid)
+// $firstname (valid)
+
+// you cannot use spaces
+
+// first name (invalid)
+
+
+// convention 
+// start with small letter and use camelCase
