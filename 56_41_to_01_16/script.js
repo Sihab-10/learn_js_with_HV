a0d52e9b007e6c4435e152077d2360c48b06c6cb
Sihab Sarar Chistee <60611053+Sihab-10@users.noreply.github.com>
2024-04-30 19:11:56 +0600

    string indexing added

---
+++ 56_41_to_01_16/script.js
@@ -0,0 +1,37 @@
+/*
+=============================================================================================
+string indexing, useful string method, typeof, string to number , number to string conversion, string concatination
+=============================================================================================
+*/ 
+
+// String indexing
+// space is also included in string indexing
+let firstName = "Sihab"
+console.log("firstName is ", firstName)
+
+
+//  s    i   h   a   b
+//  0    1   2   3   4
+
+console.log("The 3rd index of firstName string is ",firstName[3]);
+
+// length of string
+// firtName.length
+
+// last Index : length - 1
+
+console.log("the length of firstName is ", firstName.length);
+console.log("the last index value of firstName variable is ",firstName[firstName.length-1]);
+
+// String methods
+
+// trim()
+// toUpperCase()
+// toLowerCase()
+// slice
+
+let SecondName = "   Sarar   ";
+
+console.log(SecondName.length);
+firstName.trim(); 
+console.log(firstName.length)
